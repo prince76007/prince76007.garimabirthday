e37c526880312bcb95b07e6cb0d51f30550e45f1
prince saxena <prince.saxena@paloaltosoft.com>
2024-03-10 23:24:15 +0530

    1st commit

---
+++ config.js
@@ -0,0 +1,42 @@
+// Dominating Heaven, October 10, 2019 https://github.com/AJLoveChina/birthday
+var config = {
+    // The length of each sentence can be arbitrary; you can write ten or twenty sentences.
+    // Try to keep each sentence within 15 characters for better display.
+    texts: [
+        "To my dear one", // Note: The last comma at the end of each sentence must be in English! It's crucial!
+        "Beloved sweetheart", // Same as above...
+        "Today is your birthday",
+        "This is our",
+        "Third birthday together",
+        "Remember last year's birthday",
+        "We had hot pot at Wang Po's place",
+        "This year, let's have some good food",
+        "Feed my cute piggy well",
+        "And then take it away",
+        "YAMI~~",
+    ],
+    /**
+     * You can leave the 'imgs' section blank, but if you want to fill it, it must follow the format below:
+     * "Corresponding text, must be exactly the same": "Image address, you can put the image in the 'imgs' folder"
+     * For example:
+     * "Beloved sweetheart": "./imgs/xiaokeai.jpg"
+     *
+     * If you don't want an image, simply start each line with two forward slashes. For example, the image for "Today is your birthday" won't be displayed :)
+     * Tip: It's better to use square or nearly square images for a better display.
+     */
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Color",
+        balloons_flying: "Seems to be missing something",
+        cake_fadein: "Cake?",
+        light_candle: "Candles?",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
