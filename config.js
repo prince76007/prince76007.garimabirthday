dbabd7a9a530d41c80b499fd689aa4406f50c705
prince saxena <prince.saxena@paloaltosoft.com>
2024-03-11 00:28:19 +0530

    code updated

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// Dominating Heaven, October 10, 2019 https://github.com/AJLoveChina/birthday
 var config = {
-    // The length of each sentence can be arbitrary; you can write ten or twenty sentences.
-    // Try to keep each sentence within 15 characters for better display.
+    //
     texts: [
         "😇Garima", // Note: The last comma at the end of each sentence must be in English! It's crucial!
         "One of the most creative", // Same as above...
@@ -16,20 +14,9 @@ var config = {
         "~~From me Prince~~",
         "LOL"
     ],
-    /**
-     * You can leave the 'imgs' section blank, but if you want to fill it, it must follow the format below:
-     * "Corresponding text, must be exactly the same": "Image address, you can put the image in the 'imgs' folder"
-     * For example:
-     * "Beloved sweetheart": "./imgs/xiaokeai.jpg"
-     *
-     * If you don't want an image, simply start each line with two forward slashes. For example, the image for "Today is your birthday" won't be displayed :)
-     * Tip: It's better to use square or nearly square images for a better display.
-     */
     imgs: {
         "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
     },
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "Start",
         play: "Music",
